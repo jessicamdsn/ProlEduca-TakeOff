324a3c6b801e73b087f148276272c086ccdf4e36
Marcellyz <marcelly.eduarda4545@gmail.com>
2025-04-22 01:34:45 -0300

    remove: removing duplicate contact section component

--- prol_educa_app/src/app/app.component.ts
+++ prol_educa_app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CustomerInterfaceComponent } from './pages/customer-interface/customer-interface.component';
+import { HomePageComponent } from "./pages/home-page/home-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CustomerInterfaceComponent],
+  imports: [HomePageComponent],
   templateUrl: './app.component.html',
   styles: []
 })
--- prol_educa_app/src/app/pages/customer-interface/customer-interface.component.ts
+++ prol_educa_app/src/app/pages/customer-interface/customer-interface.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../layout/header/header.component";
 import { BolsasListComponent } from "../../components/bolsas-list/bolsas-list.component";
-import { ContactSectionComponent } from "../../layout/contact-section/contact-section.component";
 import { FooterComponent } from "../../layout/footer/footer.component";
+import { ContactSectionComponent } from '../../components/contact-section/contact-section.component';
 
 @Component({
   selector: 'app-customer-interface',
