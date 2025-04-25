1a649d893eb5e468731e68d992f13876f3c5e17c
Marcellyz <marcelly.eduarda4545@gmail.com>
2025-04-25 20:06:02 -0300

    feat: added add button logic

--- prol_educa_app/src/app/components/card-instituicao/card-instituicao.component.ts
+++ prol_educa_app/src/app/components/card-instituicao/card-instituicao.component.ts
@@ -2,14 +2,19 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { BtnAddComponent } from '../btn-add/btn-add.component';
 
 @Component({
   standalone: true,
   selector: 'app-card-instituicao',
   templateUrl: './card-instituicao.component.html',
   styleUrls: ['./card-instituicao.component.scss'],
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, BtnAddComponent],
 })
 export class CardInstituicaoComponent {
   @Input() titulo!: string;
+
+  @Input() isAliveBtnAdd: boolean = false;
+
+
 }
---
+++ prol_educa_app/src/app/pages/view-admin-users/view-admin-users.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { SidebarMenuComponent } from "../../components/sidebar-menu/sidebar-menu.component";
+import { HeaderAdminComponent } from '../../layout/header-admin/header-admin.component';
+import { CardInstituicaoComponent } from "../../components/card-instituicao/card-instituicao.component";
+
+@Component({
+  selector: 'app-view-admin-users',
+  standalone: true,
+  imports: [SidebarMenuComponent, HeaderAdminComponent, CardInstituicaoComponent],
+  templateUrl: './view-admin-users.component.html',
+  styleUrl: './view-admin-users.component.scss'
+})
+export class ViewAdminUsersComponent {
+
+}
