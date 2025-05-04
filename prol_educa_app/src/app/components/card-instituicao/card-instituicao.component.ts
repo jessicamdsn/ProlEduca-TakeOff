150f18967cc6f0a59a32708e2bf3597bb0406365
Marcellyz <marcelly.eduarda4545@gmail.com>
2025-05-04 03:27:23 -0300

    style: assign icon to routes on institution card

--- prol_educa_app/src/app/components/card-instituicao/card-instituicao.component.ts
+++ prol_educa_app/src/app/components/card-instituicao/card-instituicao.component.ts
@@ -5,13 +5,14 @@ import { Router } from '@angular/router';
 
 import { MatCardModule } from '@angular/material/card';
 import { BtnAddComponent } from '../btn-add/btn-add.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   standalone: true,
   selector: 'app-card-instituicao',
   templateUrl: './card-instituicao.component.html',
   styleUrls: ['./card-instituicao.component.scss'],
-  imports: [CommonModule, MatCardModule, BtnAddComponent],
+  imports: [CommonModule, MatCardModule, BtnAddComponent, MatIconModule],
 })
 export class CardInstituicaoComponent {
   @Input() titulo!: string;
@@ -31,8 +32,8 @@ export class CardInstituicaoComponent {
   getIconsName() {
     if (this.rotaAtual.includes('/admin/cursos')) return 'book';
     if (this.rotaAtual.includes('/admin/bolsistas')) return 'person';
-    if (this.rotaAtual.includes('/admin')) return 'admin_person_settings';
+    if (this.rotaAtual.includes('/admin')) return 'admin_panel_settings';
 
-    return '../../../assets/document-icon.svg';
+    return 'draft';
   }
 }
