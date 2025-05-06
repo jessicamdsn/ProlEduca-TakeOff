05cc25f21f49b90c6db8072ef6506828c3f88e50
Marcellyz <marcelly.eduarda4545@gmail.com>
2025-05-05 21:27:48 -0300

    fix: edit modal fix

--- prol_educa_app/src/app/shared/edit-user-dialog/edit-user-dialog.component.ts
+++ prol_educa_app/src/app/shared/edit-user-dialog/edit-user-dialog.component.ts
@@ -1,9 +1,14 @@
-import { CommonModule } from "@angular/common";
-import { Component, Inject } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from "@angular/material/dialog";
-import { MatFormFieldModule, MatLabel } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
+import { CommonModule } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatOptionModule } from '@angular/material/core';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-edit-dialog',
@@ -13,15 +18,15 @@ import { MatInputModule } from "@angular/material/input";
     MatDialogModule,
     MatFormFieldModule,
     MatInputModule,
-    MatLabel,
-    FormsModule
+    FormsModule,
+    MatOptionModule,
   ],
   templateUrl: './edit-user-dialog.component.html',
   styleUrls: ['./edit-user-dialog.component.scss'],
 })
 export class EditUserDialogComponent {
-  tipo: 'usuario' | 'curso';
-  campos: { label: string, name: string, type: string }[] = [];
+  tipo: 'usuario' | 'curso' | 'instituicao';
+  campos: { label: string; name: string; type: string }[] = [];
   dados: any = {};
 
   constructor(
@@ -34,9 +39,7 @@ export class EditUserDialogComponent {
   }
 
   definirCampos() {
-
     console.log('Tipo:', this.tipo);
-
 
     if (this.tipo === 'usuario') {
       this.campos = [
@@ -48,13 +51,30 @@ export class EditUserDialogComponent {
     } else if (this.tipo === 'curso') {
       this.campos = [
         { label: 'Nome do Curso', name: 'nome', type: 'text' },
-        { label: 'Código da Instituição', name: 'codInstituicao', type: 'text' },
+        {
+          label: 'Código da Instituição',
+          name: 'codInstituicao',
+          type: 'text',
+        },
         { label: 'Vagas', name: 'vagas', type: 'number' },
         { label: 'Percentual Bolsa', name: 'percentualBolsa', type: 'number' },
+      ];
+    } else if (this.tipo === 'instituicao') {
+      this.campos = [
+        { label: 'Nome da Instituicao', name: 'nome', type: 'text' },
+        { label: 'Status', name: 'status', type: 'boolean' },
+        { label: 'Tipo de Ensino', name: 'type', type: 'text' },
+        { label: 'Curso', name: 'courses', type: 'text' },
       ];
     }
   }
 
+  getUltimoCurso(): string {
+    if (Array.isArray(this.dados.courses) && this.dados.courses.length > 0) {
+      return this.dados.courses[this.dados.courses.length - 1];
+    }
+    return '';
+  }
 
   cancel(): void {
     this.dialogRef.close();
