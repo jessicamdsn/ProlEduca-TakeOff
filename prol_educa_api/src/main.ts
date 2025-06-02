017d53cd07471b8fa3c04d96fb46c30075cf077e
Jessica Neves <128631606+jessicamdsn@users.noreply.github.com>
2025-06-02 20:36:42 -0300

    refactor: changed backend files

--- prol_educa_api/src/main.ts
+++ prol_educa_api/src/main.ts
@@ -6,18 +6,17 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
 
-
-
 dotenv.config();
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-  origin: '*',  // permite qualquer origem (pode ser ajustado para um domínio específico)
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  origin: ['https://prol-educa-take-off.vercel.app/','localhost:4200'],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   preflightContinue: false,
   allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, // Se você precisar enviar cookies ou cabeçalhos de autorização
 });
   await app.listen(process.env.PORT ?? 3000);
 }
--- prol_educa_app/src/app/pages/login/login.component.ts
+++ prol_educa_app/src/app/pages/login/login.component.ts
@@ -1,13 +1,45 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { BannerLoginComponent } from '../../layout/banner-login/banner-login.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [BannerLoginComponent],
+  imports: [BannerLoginComponent,
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+loginForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) return;
+
+    const formData = this.loginForm.value;
+
+    this.http.post<any>('https://seu-endpoint.com/login', formData).subscribe({
+      next: (res) => {
+        localStorage.setItem('token', res.access_token);
+        this.router.navigate(['/admin']);
+      },
+      error: () => {
+        alert('Email ou senha inválidos');
+      }
+    });
+  }
 }
