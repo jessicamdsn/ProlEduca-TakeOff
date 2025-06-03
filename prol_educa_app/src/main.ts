fcfdcb332a5c14d3d9e919f005ddb9c709b9c92e
Marcellyz <marcelly.eduarda4545@gmail.com>
2025-06-03 00:55:04 -0300

    refactor: main.ts file

--- prol_educa_app/src/main.ts
+++ prol_educa_app/src/main.ts
@@ -1,6 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { routes } from './app/app.routes';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    ...appConfig.providers,
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimations(),
+
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+  ],
+}).catch(err => console.error(err));
